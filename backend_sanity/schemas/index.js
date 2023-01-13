fa4b59057bc509cba84f1f855acd27d153c9aeb8
Hemant soni <sonihemant4741@gmail.com>
2023-01-13 13:52:42 +0530

    Update schema on sanity

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,1 +1,16 @@
-export const schemaTypes = []
+import testimonials from './testimonials';
+import works from './works'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+export const schemaTypes = [
+  testimonials,
+  works,
+  abouts,
+  experiences,
+  skills,
+  workExperience,
+  contact
+]
